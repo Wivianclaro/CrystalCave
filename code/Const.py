a40645b11a19d1723c3817447d091e8ec12724ec
Wivian <wivianclaroestb@gmail.com>
2025-06-20 16:04:42 -0300

    Creating asset folder Inserting all assets Creating the menu: background, sound, and fonts Creating the const file

---
+++ code/Const.py
@@ -0,0 +1,14 @@
+# C
+COLOR_BLUE = (0, 35, 245)
+COLOR_WHITE = (255, 255, 255)
+
+# M
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COOPERATIVE',
+               'SCORE',
+               'EXIT')
+
+# W
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
